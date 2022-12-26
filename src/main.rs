db2001fd4e60fc981dc894d9be757f3e686d83c5
Rhelvetican <bionicvnb@gmail.com>
2022-12-26 22:04:29 +0700

    Removing redundant functions

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,5 @@ struct Sheet {
     point2: Point,
 }
 
-fn load_from_file(file_path: &str) {
-    let file = File::open(file_path).expect("file wasn't found.");
-    let reader = BufReader::new(file);
-
-    let _numbers: Vec<i64> = reader
-        .lines()
-        .map(|line| line.unwrap().parse::<i64>().unwrap())
-        .collect();
-}
-
 fn main() {
-    load_from_file("BAI5.inp");
 }
