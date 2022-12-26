1abc6fa0b8ba82f01dbc7b8184fddd5ac3e1eafa
Rhelvetican <bionicvnb@gmail.com>
2022-12-26 21:46:33 +0700

    Getting ready

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[allow(dead_code)]
+
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+struct Sheet {
+    point1: Point,
+    point2: Point,
+}
+
+fn load_from_file(file_path: &str) {
+    let file = File::open(file_path).expect("file wasn't found.");
+    let reader = BufReader::new(file);
+
+    let _numbers: Vec<i64> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<i64>().unwrap())
+        .collect();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("BAI5.inp")
+        .expect("Can't Open File!");
 }
